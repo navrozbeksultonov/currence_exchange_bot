c9c91ae1add0fb8ff7d69b9a73e30f251f34ae96
Navro`zbek Sultonov <106438432+navrozbeksultonov@users.noreply.github.com>
2022-06-05 21:04:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from datetime import datetime
 import logging
 from markup import markaMenu
-TOKEN = 'TOKEN'
+TOKEN = 'INSERT YOUR TOKEN'
 logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
